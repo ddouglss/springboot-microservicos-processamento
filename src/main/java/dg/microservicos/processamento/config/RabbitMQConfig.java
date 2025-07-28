f92e28f5f932e6677e38d0583ce0afce8dcf17e4
ddouglss <ddouglss1999@gmail.com>
2025-07-28 04:50:10 -0300

    sistema com processamento com RabbitMQ finalizado

--- src/main/java/dg/microservicos/processamento/config/RabbitMQConfig.java
+++ src/main/java/dg/microservicos/processamento/config/RabbitMQConfig.java
@@ -1,5 +1,7 @@
 package dg.microservicos.processamento.config;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,4 +18,9 @@ public class RabbitMQConfig {
         return new Queue(queue, true);
     }
 
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter (){
+        ObjectMapper objectMapper = new ObjectMapper();
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
 }
